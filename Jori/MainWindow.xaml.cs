0c30c81c3a76af1d15110648d52fba5e86879386
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-04-18 22:11:31 +0200

    write pixels, listen for keydown and also timer

--- Jori/MainWindow.xaml.cs
+++ Jori/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
+﻿using System.Data.Common;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Jori
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer _timer;
+        private WriteableBitmap _bitmap;
+
         public MainWindow()
         {
             InitializeComponent();
+            _timer = new DispatcherTimer(TimeSpan.FromMilliseconds(40), DispatcherPriority.Normal, Tick, Dispatcher);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _bitmap = new WriteableBitmap((int)ActualWidth, (int)ActualHeight, 96, 96, PixelFormats.Bgr32, null);
+            GameArea.Source = _bitmap;
+
+            byte[] tile = new byte[32 * 32 * 4]; // B G R
+            for (int i = 0; i < tile.Length; i++) { tile[i] = 128; }
+            _bitmap.WritePixels(new Int32Rect(0, 0, 32, 32), tile, 32 * 4, 0);
+
+            //_timer.Start();
+        }
+
+        private void Tick(object? sender, EventArgs e)
+        {
+
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
