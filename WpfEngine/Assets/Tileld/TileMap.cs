1ae9e275121b362f643e374c57f650486e30318d
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-04-20 11:18:06 +0200

    basc tmx parsing

---
+++ WpfEngine/Assets/Tileld/TileMap.cs
@@ -0,0 +1,54 @@
+﻿using System.Xml;
+
+namespace WpfEngine.Assets.Tileld
+{
+    public class Layer
+    {
+
+    }
+
+    public class TileSet
+    {
+
+    }
+
+    public class TileMap
+    {
+        private List<Layer> _layers;
+        private List<TileSet> _tileSets;
+
+        public TileMap(List<Layer> layers, List<TileSet> tilesets)
+        {
+            _layers = layers;
+            _tileSets = tilesets;
+        }
+
+        private static TileSet ReadTileSet(XmlElement node)
+        {
+            return new TileSet();
+        }
+        private static Layer ReadLayer(XmlElement node)
+        {
+            return new Layer();
+        }
+        internal static TileMap Load(string path)
+        {
+            var doc = new XmlDocument();
+            doc.Load(path);
+            var layers = new List<Layer>();
+            var tilesets = new List<TileSet>();
+            foreach (XmlElement node in doc.DocumentElement.ChildNodes)
+            {
+                if (node.Name == "tileset")
+                {
+                    tilesets.Add(ReadTileSet(node));
+                }
+                if (node.Name == "layer")
+                {
+                    layers.Add(ReadLayer(node));
+                }
+            }
+            return new TileMap(layers, tilesets);
+        }
+    }
+}
--- WpfEngine/Engine.cs
+++ WpfEngine/Engine.cs
@@ -1,7 +1,9 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using WpfEngine.Assets.Tileld;
 
 namespace WpfEngine
 {
@@ -14,7 +16,7 @@ namespace WpfEngine
     {
         private readonly DispatcherTimer _timer;
         private readonly WriteableBitmap _buffer;
-        private readonly List<Blittable> _blittables = new List<Blittable>();
+        private TileMap _map;
 
         public Engine(int width, int height, Dispatcher dispatcher)
         {
@@ -24,17 +26,22 @@ namespace WpfEngine
             _buffer = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgr32, null);
         }
 
+        public void LoadMap(string path)
+        {
+            _map = TileMap.Load(path);
+        }
+
+        private void RenderMap()
+        {
+        }
+
         private void Tick(object? sender, EventArgs e)
         {
             Render();
         }
 
         public WriteableBitmap Buffer { get { return _buffer; } }
-
-        public void Add(Blittable blittable)
-        {
-            _blittables.Add(blittable);
-        }
+        
 
         public void Start()
         {
@@ -52,10 +59,7 @@ namespace WpfEngine
             var pixels = new byte[_buffer.PixelWidth * _buffer.PixelHeight * 4];
             _buffer.WritePixels(new Int32Rect(0, 0, _buffer.PixelWidth, _buffer.PixelHeight), pixels, _buffer.PixelWidth * 4, 0);
 
-            foreach (var blittable in _blittables)
-            {
-                blittable.BlitTo(_buffer);
-            }
+            RenderMap();
         }
 
     }
